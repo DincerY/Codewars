4796dfcdc8be1ec1328d00e0ee034ea796423f6d
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-10-02 12:56:29 +0300

    initial commit

---
+++ Codewars.RemovingElements/Program.cs
@@ -0,0 +1,18 @@
+﻿Kata.RemoveEveryOther(new object[]{"Hello", "Goodbye", "Hello Again"});
+Kata.RemoveEveryOther(new object[] { new object[] { 1, 2 } }); 
+Kata.RemoveEveryOther(new object[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+
+Console.WriteLine("Hello, World!");
+
+public static class Kata
+{
+    public static object[] RemoveEveryOther(object[] arr)
+    {
+        var list = new List<object>();
+        for (int i = 0; i < arr.Length; i += 2)
+        {
+            list.Add(arr[i]);	
+        }
+        return list.ToArray();
+    }
+}
