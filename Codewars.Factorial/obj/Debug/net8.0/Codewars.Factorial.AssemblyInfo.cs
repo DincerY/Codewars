97c84f503dbeed7287494391e998477cb068a267
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-10-09 11:04:40 +0300

    to be continue

--- Codewars.Factorial/obj/Debug/net8.0/Codewars.Factorial.AssemblyInfo.cs
+++ Codewars.Factorial/obj/Debug/net8.0/Codewars.Factorial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Codewars.Factorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a74210c9d63c5d9014097835cbedabb2e275cc7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cd1b145e1ff599dc5fcc0fb9aac4e5a43916610")]
 [assembly: System.Reflection.AssemblyProductAttribute("Codewars.Factorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Codewars.Factorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Codewars.SmallestKElementsOfList/Program.cs
+++ Codewars.SmallestKElementsOfList/Program.cs
@@ -11,11 +11,60 @@ Console.WriteLine("Hello, World!");
 
 class SmallestK
 {
+    private static Random rand = new();
     public static T[] SmallestKInList<T>(T[] arr, int k) where T : IComparable<T>
     {
+        if (k <= 0)
+        {
+            return new T[]{};
+        }
+
+        if (k >= arr.Length)
+        {
+            return arr;
+        }
+
+        int left = 0;
+        int right = arr.Length - 1;
+        while (true)
+        {
+            int pivotIndex = Partition();
+            if (pivotIndex == k)
+            {
+                break;
+            }
+            else if (pivotIndex < k)
+            {
+                left = pivotIndex + 1;
+            }
+            else
+            {
+                right = pivotIndex - 1;
+            }
+        }
         Array.Sort(arr);
         T[] result = new T[k];
         Array.Copy(arr, result, k);
         return result;
+    }
+
+    private static int Partition<T>(T[] arr, int left, int right)
+    {
+        int pivotIndex = rand.Next(left, right + 1);
+        var pivot = arr[pivotIndex];
+        (arr[pivotIndex], arr[right]) = (arr[right], arr[pivotIndex]);
+
+        int storeIndex = left;
+
+        for (int i = left; i < right; i++)
+        {
+            if (arr[i] < pivot)
+            {
+                (arr[storeIndex], arr[i]) = (arr[i], arr[storeIndex]);
+                storeIndex++;
+            }
+        }
+        (arr[storeIndex], arr[right]) = (arr[right], arr[storeIndex]);
+        return storeIndex;
     }
 }
