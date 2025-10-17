ad0eccb50e97627d664cc272c20ed7c088058d41
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-10-17 08:25:47 +0300

    accepted

--- go/Codewars.Factorial/main.go
+++ go/Codewars.Factorial/main.go
@@ -1,5 +1,28 @@
 package main
 
 func main() {
-	return
+	Multiple3And5(100)
+}
+
+func Multiple3And5(number int) int {
+	var res int = 0
+	for i := 0; i < number; i++ {
+		if i%3 == 0 {
+			res += i
+		} else if i%5 == 0 {
+			res += i
+		} else {
+			continue
+		}
+	}
+	return res
+}
+func Multiple3And5Dif(number int) int {
+	var res int = 0
+	for i := 0; i < number; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			res += i
+		}
+	}
+	return res
 }
