af12d1def04ac62b6624cb81f92fc5ba5b0bb863
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-10-24 08:35:02 +0300

    did not solve

--- go/Codewars.SquareSums/main.go
+++ go/Codewars.SquareSums/main.go
@@ -1,9 +1,23 @@
 package main
 
 func main() {
-
+	SquareSumsRow(10)
 }
 
 func SquareSumsRow(n int) []int {
+	var square []int = []int{4, 9, 16, 25, 36, 49, 64, 81, 100}
+	var test = [][]int{}
+
+	for i := 0; i <= n; i++ {
+		test = append(test, []int{})
+	}
+	for i := 1; i <= n; i++ {
+		for _, val := range square {
+			if val-i <= n {
+				test[i] = append(test[i], 1)
+			}
+		}
+	}
+
 	return nil
 }
