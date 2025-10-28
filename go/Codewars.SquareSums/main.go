a69ea2726ff05c77ffd98a72e0b71b64f54d44a1
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-10-28 10:05:15 +0300

    to be continue for refactoring

--- go/Codewars.SquareSums/main.go
+++ go/Codewars.SquareSums/main.go
@@ -3,51 +3,38 @@ package main
 import "math"
 
 func main() {
+
 	SquareSumsRow(10)
 }
 
-func SquareSumsRow(n int) []int {
-	res := make([]int, 0, n)
-	usedMap := make(map[int]struct{})
-	if DFS(&res,usedMap,n) {
-		return res
-	}
-	return nil;
-}
+var (
+	found bool
+	res   []int
+	used  []bool
+)
 
-func isPerfectSquare(num int) bool {
-	if num < 0 {
-		return false
-	}
-	sqrt := int(math.Sqrt(float64(num)))
-	return sqrt*sqrt == num
-}
-
-func DFS(r *[]int, used map[int]struct{}, n int) bool {
-	if len(used) == n {
+func isSquare(val int) bool {
+	sqrt := int(math.Sqrt(float64(val)))
+	if val > 0 && sqrt*sqrt == val {
 		return true
 	}
-
-	for i := 1; i <= n; i++ {
-		if _, exists := used[i]; exists {
-			continue
-		}
-		if len(*r) > 0 {
-			lastElement := (*r)[len(*r)-1]
-			if !isPerfectSquare(lastElement + i) {
-				continue 
-			}
-		}
-
-		used[i] = struct{}{}
-		*r = append(*r, i)  
-
-		if DFS(r, used, n) {
-			return true
-		}
-
-		delete(used, i)      
-		*r = (*r)[:len(*r)-1] 
-	}
 	return false
+}
+
+func dfs(step int){
+	for i := 0; i < n; i++ {
+		if step == 1 || !used[i] && isSquare(res[len(res)-1] + i){
+			res, used[i] = append(res, i), true
+			dfs()
+		}
+	}
+}
+
+func SquareSumsRow(n int) []int{
+	used = make([]bool, n+1)
+	res = make([]int, 0)
+	
+
+
+	return nil
 }
