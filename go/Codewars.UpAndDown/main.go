09f349353507e6b847d97f124e7db8339408c5fc
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-11-03 08:59:34 +0300

    accepted

--- go/Codewars.UpAndDown/main.go
+++ go/Codewars.UpAndDown/main.go
@@ -11,27 +11,30 @@ func main() {
 }
 
 func Arrange(s string) string {
-	strs := strings.Split(s, " ")
-
-	last := 0
-
+	strs := strings.Fields(s)
 	
 	//strs[1], strs[2] = strs[2], strs[1]
 	//0,2,4,6,8,10 indexes is lower - (even)
 	//1,3,5,7,9,11 indexes is upper - (odd)
-	for i := 1; i < len(strs)-1; i++ {	
+
+	for i := 0; i < len(strs)-1; i++ {	
 		if i % 2 == 0 {
-			//lower
-			if strs[last] < strs[i] {
-				strs[last] , strs[i] = strs[i], strs[last] 
+			if len(strs[i]) > len(strs[i+1]) {
+				strs[i] , strs[i+1] = strs[i+1], strs[i] 
 			}
-			last++
 		}else{
-			//upper
-
-
-			last++
+			if len(strs[i]) < len(strs[i+1]) {
+				strs[i] , strs[i+1] = strs[i+1], strs[i] 
+			}
 		}
 	}
-	return ""
+	for i,_ := range strs{
+		if i % 2 == 1 {
+			strs[i] = strings.ToUpper(strs[i])
+			
+		}else{
+			strs[i] = strings.ToLower(strs[i])
+		}
+	}
+	return strings.Join(strs, " ")
 }
