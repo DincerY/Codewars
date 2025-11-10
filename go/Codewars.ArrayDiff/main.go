340855e30f44849c4b7c58cce383e08027e53b5d
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-11-10 10:09:49 +0300

    accepted

--- go/Codewars.ArrayDiff/main.go
+++ go/Codewars.ArrayDiff/main.go
@@ -8,23 +8,21 @@ func main() {
 	ArrayDiff([]int{1, 2, 2}, emptyArr)
 }
 
-func contains(arr []int, val int) (bool, int) {
-	for i, v := range arr {
+func contains(arr []int, val int) bool {
+	for _, v := range arr {
 		if v == val {
-			return true, i
+			return true
 		}
 	}
-	return false, -1
+	return false
 }
 
 func ArrayDiff(a, b []int) []int {
-	//it is working one time. If you want to remove two times
-	//It won't work.
-	for i := 0; i < len(b); i++ {
-		if contain, index := contains(a, b[i]); contain {
-			a = append(a[:index], a[index+1:]...)
+	var res []int
+	for i := 0; i < len(a); i++ {
+		if !contains(b, a[i]) {
+			res = append(res, a[i])
 		}
 	}
-
-	return a
+	return res
 }
