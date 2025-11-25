cbf3d52d13ec318ac984a991497329b05766e9cb
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-11-25 09:58:13 +0300

    to be continue

--- go/Codewars.Snail/main.go
+++ go/Codewars.Snail/main.go
@@ -2,26 +2,48 @@ package main
 
 func main() {
 	snailMap := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
-	Snail(snailMap)
+	/*Snail(snailMap)
 	snailMap = [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}}
+	Snail(snailMap)*/
+
+	snailMap = [][]int{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}}
 	Snail(snailMap)
 }
 
 func Snail(snaipMap [][]int) []int {
+	if len(snaipMap) == 0 {
+		return nil
+	}
 	row := len(snaipMap)
 	column := len(snaipMap[0])
 
-	println(column)
-	println(row)
+	var res []int
+	Recursive(0, 0, row-1, column-1, &res, snaipMap)
 
-	return []int{}
+	return res
 }
 
-func Recursive() {
+// helper func
+func contains(arr []int, val int) bool {
+	for _, v := range arr {
+		if v == val {
+			return true
+		}
+	}
+	return false
+}
 
-	Recursive() // right
-	Recursive() // down
-	Recursive() // left
-	Recursive() // up
+func Recursive(x, y, row, col int, res *[]int, snailMap [][]int) {
 
+	if x > row || x < 0 || y > col || y < 0 || contains(*res, snailMap[x][y]) {
+		return
+	} else {
+		*res = append(*res, snailMap[x][y])
+	}
+	if x-1 < 0 || contains(*res, snailMap[x-1][y]) {
+		Recursive(x, y+1, row, col, res, snailMap) // right
+	}
+	Recursive(x+1, y, row, col, res, snailMap) // down
+	Recursive(x, y-1, row, col, res, snailMap) // left
+	Recursive(x-1, y, row, col, res, snailMap) // up
 }
