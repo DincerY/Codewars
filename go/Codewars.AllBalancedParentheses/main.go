54bba73d2cbb86b1374cbc96e7eb7d806dcbae76
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-12-08 10:03:22 +0300

    to be continue

--- go/Codewars.AllBalancedParentheses/main.go
+++ go/Codewars.AllBalancedParentheses/main.go
@@ -1,33 +1,49 @@
 package main
 
 func main() {
-	BalancedParens(4)
-
 	BalancedParens(0)
 	BalancedParens(1)
 	BalancedParens(2)
 	BalancedParens(3)
-
+	BalancedParens(4)
 }
+
+var result []string
 
 func BalancedParens(n int) []string {
-	Recursive("", n, n)
-	return nil
+	if n == 0 {
+		return []string{""}
+	}
+	Recursive("", n, n, n*2)
+	return result
 }
 
-var test []string
+func isValid(res string) bool {
+	var stack []rune
+	for _, chr := range res {
+		if chr == ')' {
+			if len(stack) == 0 {
+				return false
+			}
+			stack = stack[:len(stack)-1]
 
-func Recursive(res string, left int, right int) {
+		} else {
+			stack = append(stack, chr)
+		}
+	}
+	return len(stack) == 0
+}
 
+func Recursive(res string, left int, right int, resLen int) {
 	if left > 0 {
-		Recursive(res+"(", left-1, right)
-		left++
+		Recursive(res+"(", left-1, right, resLen)
 	}
 	if right > 0 {
-		Recursive(res+")", left, right-1)
-		right++
+		Recursive(res+")", left, right-1, resLen)
 	}
 	if left == 0 && right == 0 {
-		test = append(test, res)
+		if len(res) == resLen && isValid(res) {
+			result = append(result, res)
+		}
 	}
 }
