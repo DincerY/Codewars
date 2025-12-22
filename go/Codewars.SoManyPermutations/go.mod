8fd3f8d7ee305de6f5e4e37af2a2c3c92ecca0a9
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-12-22 09:57:48 +0300

    to be continue

---
+++ go/Codewars.SoManyPermutations/go.mod
@@ -0,0 +1,3 @@
+module SoManyPermutations
+
+go 1.25.4
---
+++ go/Codewars.SoManyPermutations/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	Permutations("a")
+	Permutations("ab")
+	Permutations("abc")
+	Permutations("aabb")
+	Permutations("abcd")
+}
+
+func Permutations(s string) []string {
+	// your code here
+	return nil
+}
