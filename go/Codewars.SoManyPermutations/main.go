0322c2c101442f5e825668606a26853ff55832f2
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-12-23 09:49:14 +0300

    to be continue

--- go/Codewars.SoManyPermutations/main.go
+++ go/Codewars.SoManyPermutations/main.go
@@ -1,14 +1,26 @@
 package main
 
 func main() {
+	Permutations("abc")
 	Permutations("a")
 	Permutations("ab")
-	Permutations("abc")
 	Permutations("aabb")
 	Permutations("abcd")
 }
 
+// working on res will get understading solution
+var res []string
+
 func Permutations(s string) []string {
-	// your code here
+	Recursion(s)
 	return nil
+}
+
+func Recursion(str string) {
+	for i := 0; i < len(str); i++ {
+		res = append(res, string(str))
+		Recursion(str[:i] + str[i+1:])
+		Recursion(str[:i] + str[i+1:])
+	}
+
 }
