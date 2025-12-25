27078024ec6ca5ca8504402d377ccc14c0b3607d
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-12-25 09:32:45 +0300

    accepted

--- go/Codewars.SoManyPermutations/main.go
+++ go/Codewars.SoManyPermutations/main.go
@@ -1,26 +1,54 @@
 package main
 
 func main() {
+	Permutations("aa")
+
+	Permutations("ab")
+
 	Permutations("abc")
 	Permutations("a")
-	Permutations("ab")
 	Permutations("aabb")
 	Permutations("abcd")
 }
 
-// working on res will get understading solution
-var res []string
+var resMap map[string]struct{}
 
 func Permutations(s string) []string {
-	Recursion(s)
-	return nil
+	resMap = make(map[string]struct{})
+	Recursion(s, "", len(s))
+	var result []string
+	for k := range resMap {
+		result = append(result, k)
+	}
+	return result
 }
 
-func Recursion(str string) {
-	for i := 0; i < len(str); i++ {
-		res = append(res, string(str))
-		Recursion(str[:i] + str[i+1:])
-		Recursion(str[:i] + str[i+1:])
+func Recursion(str string, temp string, lenght int) {
+	if len(temp) == lenght {
+		resMap[temp] = struct{}{}
+		return
 	}
+	for i := 0; i < len(str); i++ {
+		Recursion(str[:i]+str[i+1:], temp+string(str[i]), lenght)
+		Recursion(str[:i]+str[i+1:], temp, lenght)
+	}
+}
 
+// It is not mine
+func Permutations2(s string) (a []string) {
+	if len(s) < 2 {
+		return []string{s}
+	}
+	m := map[string]bool{}
+	for _, sub := range Permutations(s[1:]) {
+		for i := 0; i <= len(sub); i++ {
+			st := sub[0:i] + s[0:1] + sub[i:]
+			if m[st] {
+				continue
+			}
+			m[st] = true
+			a = append(a, st)
+		}
+	}
+	return
 }
