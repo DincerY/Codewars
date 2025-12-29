f69dbcf425b3e6eed1d1725be3469b3c6a1839e1
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-12-29 09:50:52 +0300

    to be continue

---
+++ go/Codewars.HammingNumbers/go.mod
@@ -0,0 +1,3 @@
+module HammingNumbers
+
+go 1.25.4
---
+++ go/Codewars.HammingNumbers/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	Hammer(1)
+	Hammer(2)
+	Hammer(3)
+	Hammer(4)
+	Hammer(5)
+	Hammer(6)
+	Hammer(7)
+	Hammer(8)
+	Hammer(9)
+	Hammer(10)
+	Hammer(11)
+	Hammer(12)
+	Hammer(13)
+	Hammer(14)
+	Hammer(15)
+}
+
+func Hammer(n int) uint {
+	// your code here
+	return 1
+}
