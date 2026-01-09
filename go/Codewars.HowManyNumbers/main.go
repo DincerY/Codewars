6f6005bdfb977a4709171de95e11e19ec154e7b8
Dinçer Yiğit <dinceryigit60@gmail.com>
2026-01-09 09:06:26 +0300

    to be continue

--- go/Codewars.HowManyNumbers/main.go
+++ go/Codewars.HowManyNumbers/main.go
@@ -1,7 +1,7 @@
 package main
 
 func main() {
-	SelNumber(20, 2)
+	SelNumber(4000, 2)
 	SelNumber(47, 3)
 }
 
@@ -10,15 +10,23 @@ func SelNumber(n, d int) int {
 	if n < 12 {
 		return 0
 	}
-	for i := 12; i <= n; i++ {
+	for i := 3456; i <= n; i++ {
 		j := i
-		var digit []int
+		digit := []int{}
 		for j > 0 {
 			number := j % 10
 			digit = append([]int{number}, digit...)
 			j = j / 10
 		}
-		if true {
+		isValid := false
+		for i := len(digit) - 1; i > 0; i-- {
+			if digit[i]-digit[i-1] <= d && digit[i]-digit[i-1] > 0 && digit[i] != digit[i-1] {
+				isValid = true
+			} else {
+				isValid = false
+			}
+		}
+		if isValid {
 			res++
 		}
 	}
