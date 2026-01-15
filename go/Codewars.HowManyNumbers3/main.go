bc4659e251604357c36098370a9779fdfe48c386
Dinçer Yiğit <dinceryigit60@gmail.com>
2026-01-15 10:27:58 +0300

    accepted

--- go/Codewars.HowManyNumbers3/main.go
+++ go/Codewars.HowManyNumbers3/main.go
@@ -1,46 +1,38 @@
 package main
 
 func main() {
-	Recursion(0, 0, 0, 3, 0, 10)
-
+	FindAll(84, 4)
+	FindAll(10, 3)
+	FindAll(27, 3)
 }
+
+var result []int
 
 func FindAll(sumDig, digs int) []int {
-	res := []int{}
-
-	//this code only use with 3 digits. I will solve it
-	for i := 1; i < 9; i++ {
-		total := 0
-		total += i
-		for j := i; j < 9; j++ {
-			total += j
-			for k := j; k < 9; k++ {
-				total += k
-				if total == sumDig {
-
-				}
-			}
-		}
+	result = []int{}
+	Recursion(1, 0, digs, sumDig, -1)
+	if len(result) == 0 {
+		return []int{}
 	}
-
-	//return [amount, min, max]
-	return []int{len(res), res[0], res[len(res)-1]}
+	return []int{len(result), result[0], result[len(result)-1]}
 }
 
-// I should change parameters
-func Recursion(digitCount, prevVal, sumDig, digs, test, resDig int) {
-	for i := 1; i < 10; i++ {
-		if digitCount < digs {
-			if test == 0 {
+func Recursion(lastDigit, digitCount, expectedDigs, sumDig, test int) {
+	for i := lastDigit; i < 10; i++ {
+		if digitCount < expectedDigs {
+			if test == -1 {
 				test = 1
 			} else {
 				test = test*10 + i
 			}
-			if sumDig == resDig {
-				//result+1
-			}
-			Recursion(digitCount+1, i, sumDig+i, digs, test, resDig)
+
+			Recursion(i, digitCount+1, expectedDigs, sumDig-i, test)
 			test /= 10
+		} else if digitCount == expectedDigs {
+			if sumDig == 0 {
+				result = append(result, test)
+			}
+			break
 		} else {
 			break
 		}
