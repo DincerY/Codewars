bdbd2c34f5f9ba39cf795efef49b11cf0a32dafb
Dinçer Yiğit <dinceryigit60@gmail.com>
2026-01-19 10:16:01 +0300

    accepted

--- go/Codewars.Beeramid/main.go
+++ go/Codewars.Beeramid/main.go
@@ -1,26 +1,32 @@
 package main
 
 func main() {
+	Beeramid(-1, 4)
+	Beeramid(2, 1.9)
+
+	Beeramid(21, 1.5)
+	Beeramid(1500, 2)
+
 	Beeramid(9, 2)
 	Beeramid(9, 1.5)
-	Beeramid(-1, 4)
 
 }
 
 func Beeramid(bonus int, price float64) int {
-	if bonus <= 0 {
+	if bonus <= 0 || price <= 0 {
 		return 0
 	}
-	levels := 1
+	levels := 0
 
 	can := int(float64(bonus) / price)
-	remainder := can
 
-	for i := 1; i < remainder; i++ {
-		levels++
+	for i := 1; i <= can; i++ {
 		square := i * i
-		if square < remainder {
-			remainder -= square
+		if square <= can {
+			levels++
+			can -= square
+		} else {
+			break
 		}
 	}
 
