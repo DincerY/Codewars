eccfbb263a2990e2ade9d90e02975f366e2cd953
Dinçer Yiğit <dinceryigit60@gmail.com>
2026-01-22 09:46:14 +0300

    to be continue

--- go/Codewars.WeightForWeight/main.go
+++ go/Codewars.WeightForWeight/main.go
@@ -8,6 +8,7 @@ func main() {
 	sumDigs("10")
 	sumDigs("21")
 
+	OrderWeight("56 65 74 100 99 68 86 180 90")
 	OrderWeight("103 123 4444 99 2000")
 	OrderWeight("2000 10003 1234000 44444444 9999 11 11 22 123")
 
@@ -21,14 +22,42 @@ func sumDigs(value string) int {
 	return sum
 }
 
-var dic map[int][]int = make(map[int][]int)
+var dic map[int][]string = make(map[int][]string)
 
 func OrderWeight(strng string) string {
+	var res string
 	values := strings.Split(strng, " ")
 	//len := len(values)
 
+	//while append is being done, I should sort array
 	for _, value := range values {
+		dic[sumDigs(value)] = append(dic[sumDigs(value)], value)
 		print(value, sumDigs(value))
 	}
-	return ""
+
+	for i := 0; i < len(dic); i++ {
+		minKey := 21000000
+		for key := range dic {
+			if key < minKey {
+				minKey = key
+			}
+		}
+		values := dic[minKey]
+		if len(values) > 1 {
+			for i := 0; i < len(values); i++ {
+				minStr := values[i]
+				for _, str := range values {
+					if str[0] < minStr[0] {
+						minStr = str
+					}
+				}
+				values = values[1:len(values)]
+				res += minStr
+			}
+		} else {
+			res += values[0]
+		}
+		delete(dic, minKey)
+	}
+	return res
 }
