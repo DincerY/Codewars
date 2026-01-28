004488d1ec86c7ce0b44918c13e007e493d7364f
Dinçer Yiğit <dinceryigit60@gmail.com>
2026-01-28 09:48:00 +0300

    to be continue

--- go/Codewars.FactorialDecomposition/main.go
+++ go/Codewars.FactorialDecomposition/main.go
@@ -1,19 +1,20 @@
 package main
 
 func main() {
+	Decomp(22)
+
 	Decomp(5)
 	Decomp(12)
-	Decomp(22)
 	Decomp(25)
 }
 
-func helper(val, divisor int) int {
+func helper(val, divisor int) (int, int) {
 	res := 0
 	for val%divisor == 0 {
 		val /= divisor
 		res++
 	}
-	return res
+	return res, val
 }
 
 func Decomp(n int) string {
@@ -22,11 +23,24 @@ func Decomp(n int) string {
 	for i := 2; i <= n; i++ {
 		arr = append(arr, i)
 	}
-	powerCount := 0
+	//powerCount := 0
 	value := arr[0]
+	resMap := map[int]int{}
 
 	for len(arr) > 0 {
+		for arr[0] == 1 {
+			arr = arr[1:]
+		}
 
+		prime := arr[0]
+		for i := 0; i < len(arr); i++ {
+			if arr[i] == 1 {
+				continue
+			}
+			power, remainder := helper(arr[i], prime)
+			resMap[prime] += power
+			arr[i] = remainder
+		}
 	}
 
 	res += string(value)
