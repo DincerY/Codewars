66952881f3ceda0c8ee47f523bc66018a60b7313
Dinçer Yiğit <dinceryigit60@gmail.com>
2026-02-20 10:05:01 +0300

    to be continue

--- go/Codewars.SumOfIntervals/main.go
+++ go/Codewars.SumOfIntervals/main.go
@@ -1,34 +1,57 @@
 package main
 
 func main() {
+	SumOfIntervals([][2]int{{1, 4}, {7, 10}, {3, 5}})
+
 	SumOfIntervals([][2]int{{1, 5}})
 	SumOfIntervals([][2]int{{1, 5}, {6, 10}})
 	SumOfIntervals([][2]int{{1, 5}, {1, 5}})
-	SumOfIntervals([][2]int{{1, 4}, {7, 10}, {3, 5}})
 
 }
 
-func helper(intervals [][2]int, val int) int {
-	index := -1
-	for _, inter := range intervals {
-		if inter[1] > val {
-			return index
+func helper(intervals [][2]int, interval [2]int) []int {
+	res := []int{}
+	for index, inter := range intervals {
+		if interval[0] < inter[0] && interval[1] > inter[0] {
+			res = append(res, index)
 		}
 	}
-	return index
+	return res
 }
 
+// func helper(intervals [][2]int, val int) []int {
+// 	res := []int{}
+// 	for index, inter := range intervals {
+// 		if inter[0] < val && inter[1] > val {
+// 			res = append(res, index)
+// 		}
+// 	}
+// 	return res
+// }
+
 func SumOfIntervals(intervals [][2]int) int {
-	arr := [][2]int{}
 	res := 0
 
 	for _, inter := range intervals {
-		print(inter[0], inter[1])
-		if len(arr) == 0 {
-			arr = append(arr, inter)
-		}
+		test := helper(intervals, inter)
+		print(test)
 
 	}
 
 	return res
 }
+
+// func SumOfIntervals2(intervals [][2]int) int {
+// 	arr := [][2]int{}
+// 	res := 0
+
+// 	for _, inter := range intervals {
+// 		print(inter[0], inter[1])
+// 		if len(arr) == 0 {
+// 			arr = append(arr, inter)
+// 		}
+
+// 	}
+
+// 	return res
+// }
