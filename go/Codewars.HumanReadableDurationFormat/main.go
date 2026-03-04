fd5f6cf4dcd79d720ed17840ba2883ac9312ab8d
Dinçer Yiğit <dinceryigit60@gmail.com>
2026-03-04 10:01:53 +0300

    accepted

--- go/Codewars.HumanReadableDurationFormat/main.go
+++ go/Codewars.HumanReadableDurationFormat/main.go
@@ -3,34 +3,42 @@ package main
 import "fmt"
 
 func main() {
-	FormatDuration(31622400)
+	FormatDuration(1)
 	FormatDuration(3662)
-
+	FormatDuration(62)
+	FormatDuration(31622400)
 }
 
 func helper(seconds int64) map[string]int64 {
 	lib := map[string]int64{
-		"seconds": 0,
-		"minutes": 0,
-		"hours":   0,
-		"days":    0,
 		"years":   0,
+		"days":    0,
+		"hours":   0,
+		"minutes": 0,
+		"seconds": 0,
 	}
 	if seconds >= 60 {
 		minutes := seconds / 60
 		lib["seconds"] = seconds % 60
+		lib["minutes"] = minutes
 		if minutes >= 60 {
 			hour := minutes / 60
 			lib["minutes"] = minutes % 60
+			lib["hours"] = hour
 			if hour >= 24 {
 				days := hour / 24
 				lib["hours"] = hour % 24
+				lib["days"] = days
 
 				if days >= 365 {
 					lib["years"] = days / 365
 					lib["days"] = days % 365
 				}
 			}
+		}
+	} else {
+		if seconds > 0 {
+			lib["seconds"] = seconds
 		}
 	}
 	return lib
@@ -42,13 +50,42 @@ func FormatDuration(seconds int64) string {
 		return "now"
 	}
 	lib := helper(seconds)
-	for k, v := range lib {
+	count := 0
+	for _, v := range lib {
+		if v > 0 {
+			count++
+		}
+	}
+	str := []string{
+		"years",
+		"days",
+		"hours",
+		"minutes",
+		"seconds"}
+	for _, k := range str {
+		v := lib[k]
 		if v == 1 {
-			res += fmt.Sprintf("%d %s", v, k[:len(k)-1])
-			print(res)
-
+			if count > 2 {
+				res += fmt.Sprintf("%d %s, ", v, k[:len(k)-1])
+			}
+			if count == 2 {
+				res += fmt.Sprintf("%d %s and ", v, k[:len(k)-1])
+			}
+			if count == 1 {
+				res += fmt.Sprintf("%d %s", v, k[:len(k)-1])
+			}
+			count--
 		} else if v > 1 {
-
+			if count > 2 {
+				res += fmt.Sprintf("%d %s, ", v, k)
+			}
+			if count == 2 {
+				res += fmt.Sprintf("%d %s and ", v, k)
+			}
+			if count == 1 {
+				res += fmt.Sprintf("%d %s", v, k)
+			}
+			count--
 		}
 	}
 
