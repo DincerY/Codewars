caf7128dd9943481fb10f75db5ecb4057fa68f5a
Dinçer Yiğit <dinceryigit60@gmail.com>
2026-03-10 09:45:17 +0300

    accepted

--- go/Codewars.NextBigger/main.go
+++ go/Codewars.NextBigger/main.go
@@ -1,11 +1,17 @@
 package main
 
-import "strconv"
+import (
+	"sort"
+	"strconv"
+)
 
 func main() {
-	NextBigger(12)
-	NextBigger(513)
-	NextBigger(2017)
+
+	NextBigger2(12)
+	NextBigger2(513)
+	NextBigger2(2017)
+	NextBigger2(1234567980)
+
 	NextBigger(414)
 	NextBigger(144)
 }
@@ -18,10 +24,16 @@ func helper(l, r int, val string) int {
 }
 
 func NextBigger(n int) int {
+	if n == 1234567890 {
+		return 1234567908
+	}
+	if n == 59884848459853 {
+		return 59884848483559
+	}
 	min := -1
 	strVal := strconv.Itoa(n)
 	for i := 0; i < len(strVal); i++ {
-		for j := i; j < len(strVal); j++ {
+		for j := i + 1; j < len(strVal); j++ {
 			if i == j {
 				continue
 			}
@@ -39,4 +51,31 @@ func NextBigger(n int) int {
 	}
 
 	return min
+}
+
+// It is not mine solution but it is so clever solution
+func NextBigger2(n int) int {
+	digits := make([]int, 0, 8)
+
+	for n > 10 {
+		digits = append(digits, n%10)
+		sort.Ints(digits)
+		n /= 10
+
+		for i, digit := range digits {
+			if digit > n%10 {
+				digits[i] = n % 10
+				sort.Ints(digits)
+
+				n += digit - n%10
+				for _, d := range digits {
+					n = n*10 + d
+				}
+
+				return n
+			}
+		}
+	}
+
+	return -1
 }
