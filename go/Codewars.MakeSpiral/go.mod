963c402ddc290598422b70db9376095e4bd265de
Dinçer Yiğit <dinceryigit60@gmail.com>
2026-03-11 10:02:53 +0300

    to be continue

---
+++ go/Codewars.MakeSpiral/go.mod
@@ -0,0 +1,3 @@
+module MakeSpiral
+
+go 1.25.4
---
+++ go/Codewars.MakeSpiral/main.go
@@ -0,0 +1,35 @@
+package main
+
+func main() {
+	Spiralize(5)
+	Spiralize(10)
+}
+
+func Spiralize(size int) [][]int {
+	res := make([][]int, size)
+	for i := 0; i < size; i++ {
+		res[i] = make([]int, size)
+	}
+	left, top := 0, 0
+	right, bottom := size-1, size-1
+	for left <= right && top <= bottom {
+		for i := 0; i < right; i++ {
+			res[top][i] = 1
+		}
+		top += 2
+		for i := 0; i < bottom; i++ {
+			res[i][right] = 1
+		}
+		right -= 2
+		for i := right; i > left; i-- {
+			res[bottom][i] = 1
+		}
+		bottom -= 2
+		for i := bottom; i > top; i-- {
+			res[i][left] = 1
+		}
+		left += 2
+	}
+
+	return [][]int{}
+}
