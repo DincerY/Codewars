5aebfdde1ed175d8ab5265a9eaaee18917845d4a
Dinçer Yiğit <dinceryigit60@gmail.com>
2026-03-31 09:23:13 +0300

    accepted

--- go/Codewars.LastDigitHugeNumber/main.go
+++ go/Codewars.LastDigitHugeNumber/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "math"
-
 func main() {
 	LastDigit([]int{937640, 767456, 981242})
 
@@ -17,72 +15,64 @@ func main() {
 
 }
 
-func modPow(base, exp, mod int) int {
-	result := 1
-	base %= mod
-
-	for exp > 0 {
-		if exp%2 == 1 {
-			result = (result * base) % mod
+func cPow(a, b, m int) int {
+	if b == 0 {
+		if 1 < m {
+			return 1
 		}
-		base = (base * base) % mod
-		exp /= 2
+		return 1%m + m
 	}
-	return result
+
+	if a == 0 {
+		return 0
+	}
+
+	if a >= m {
+		a = (a % m) + m
+	}
+
+	res := 1
+	for i := 0; i < b; i++ {
+		res *= a
+		if res >= m {
+			res = (res % m) + m
+		}
+	}
+	return res
 }
 
-func LastDigit(arr []int) int {
-	if len(arr) == 0 {
+func modFor(i int) int {
+	switch i {
+	case 0:
+		return 10
+	case 1:
+		return 4
+	case 2:
+		return 2
+	default:
 		return 1
+	}
+}
+
+// LastDigit, kuvvet kulesinin son basamağını hesaplar
+func LastDigit(as []int) int {
+	n := len(as)
+
+	if n == 0 {
+		return 1
+	}
+
+	for i := 3; i < n; i++ {
+		if as[i] > 0 {
+			n = i + 1
+			break
+		}
 	}
 
 	exp := 1
-
-	for i := len(arr) - 1; i >= 1; i-- {
-		base := arr[i]
-
-		if exp == 0 {
-			exp = 1
-		}
-
-		exp = modPow(base, exp, 4)
-
-		if exp == 0 {
-			exp = 4
-		}
+	for i := n - 1; i >= 0; i-- {
+		exp = cPow(as[i], exp, modFor(i))
 	}
 
-	return modPow(arr[0], exp, 10)
-}
-
-func LastDigit2(as []int) int {
-	if len(as) == 0 {
-		return 1
-	}
-	if len(as) == 1 {
-		return as[0] % 10
-	}
-
-	floatAs := []float64{}
-
-	for _, val := range as {
-		floatAs = append(floatAs, float64(val))
-	}
-
-	for i := len(floatAs) - 2; i > 0; i-- {
-		floatAs[i] = math.Pow(floatAs[i], floatAs[i+1])
-	}
-
-	power := int(floatAs[1])
-	val := int(floatAs[0])
-	lastDigit := val % 10
-	if power == 0 {
-		return 1
-	}
-
-	for i := 0; i < power-1; i++ {
-		lastDigit = (lastDigit * val) % 10
-
-	}
-	return lastDigit
+	return exp % 10
 }
