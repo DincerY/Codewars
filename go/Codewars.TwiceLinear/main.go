4b2f87e8381eb448bf4ec5f3ff88f75b68de3df5
Dinçer Yiğit <dinceryigit60@gmail.com>
2026-04-02 09:34:59 +0300

    to be continue

--- go/Codewars.TwiceLinear/main.go
+++ go/Codewars.TwiceLinear/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"sort"
+)
+
 func main() {
 	DblLinear(10)
 	DblLinear(50)
@@ -7,20 +11,33 @@ func main() {
 
 }
 
+// It worked but we got timed out error
 func DblLinear(n int) int {
 	res := []int{1}
 	dic := make(map[int]struct{})
 	dic[1] = struct{}{}
 
 	for true {
-		i := 1
-		if dic[i] == struct{}{} {
+		i := res[0]
+		res = res[1:]
+		res = append(res, (i*2)+1)
+		res = append(res, (i*3)+1)
+		sort.Ints(res)
+
+		if _, exists := dic[i]; exists {
 			continue
 		}
-		if i == n {
+		if len(dic) == n+1 {
 			break
 		}
-		i++
+		dic[i] = struct{}{}
 	}
-	return res[len(res)-1]
+	max := 0
+
+	for k := range dic {
+		if k > max {
+			max = k
+		}
+	}
+	return max
 }
